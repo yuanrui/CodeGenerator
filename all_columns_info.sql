d6f83dc095d565d29cc8c047b2b2430196ecc8ff
yuanrui <cqdeveloper@hotmail.com>
2015-10-15 15:54:26 +0800

    修复bug
    解决当主键同时为外键时，会出现两行数据的bug

--- all_columns_info.sql
+++ all_columns_info.sql
@@ -7,14 +7,18 @@ GO
 -- Description:	查看当前数据库的数据表中每一列的具体信息
 -- ==============================================================
 CREATE VIEW all_columns_info
-as
-select a.object_id as TableID, a.name as TableName, b.column_id as ID, b.name as Name, d.value as Comment , c.name as DBType, b.max_length as 'Length',
-'IsPrimaryKey' = case when f.CONSTRAINT_TYPE = 'PRIMARY KEY' then CAST(1 as bit) else CAST(0 as bit) end, 'IsForeignKey'=case when f.CONSTRAINT_TYPE = 'FOREIGN KEY' then CAST(1 as bit) else CAST(0 as bit) end, b.IS_NULLABLE as 'IsNullAble'
-from sys.tables a 
-inner join sys.columns b on a.object_id = b.object_id
-inner join sys.types c on b.user_type_id = c.user_type_id
-left join sys.extended_properties d on a.object_id = d.major_id and d.minor_id = b.column_id
-left join INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE e on a.name = e.TABLE_NAME and b.name = e.COLUMN_NAME
-left join INFORMATION_SCHEMA.TABLE_CONSTRAINTS f on e.CONSTRAINT_NAME = f.CONSTRAINT_NAME
+AS
+WITH colConsCTE(TABLE_NAME, COLUMN_NAME, CONSTRAINT_TYPE) AS (
+     SELECT A.TABLE_NAME, A.COLUMN_NAME, B.CONSTRAINT_TYPE FROM INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE A
+     LEFT JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS B ON A.CONSTRAINT_NAME = B.CONSTRAINT_NAME AND A.TABLE_NAME = B.TABLE_NAME 
+)
+SELECT a.object_id AS TableID, a.name AS TableName, b.column_id AS ID, b.name as Name, d.value AS Comment , c.name AS DBType, b.max_length AS 'Length',
+	CASE WHEN (SELECT COUNT(1) FROM colConsCTE cte WHERE b.name = cte.COLUMN_NAME AND a.name = cte.TABLE_NAME AND cte.CONSTRAINT_TYPE ='PRIMARY KEY') > 0 THEN 1 ELSE 0 END AS IsPrimaryKey,
+	CASE WHEN (SELECT COUNT(1) FROM colConsCTE cte WHERE b.name = cte.COLUMN_NAME AND a.name = cte.TABLE_NAME AND cte.CONSTRAINT_TYPE ='FOREIGN KEY') > 0 THEN 1 ELSE 0 END AS IsForeignKey,
+	b.IS_NULLABLE as 'IsNullAble'
+FROM sys.tables a 
+LEFT JOIN sys.columns b on a.object_id = b.object_id
+LEFT JOIN sys.types c on b.user_type_id = c.user_type_id
+LEFT JOIN sys.extended_properties d on a.object_id = d.major_id and d.minor_id = b.column_id
 
 GO
