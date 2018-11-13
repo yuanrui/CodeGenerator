29d41d0ee16fbaf0a8fe5dbb9bb4aa6e39f4f717
yuanrui <yuanrui@live.cn>
2018-11-13 09:43:00 +0800

    finish mysql schema.

--- Banana.AutoCode/DbSchema/Provider/MySqlSchema.cs
+++ Banana.AutoCode/DbSchema/Provider/MySqlSchema.cs
@@ -47,23 +47,128 @@ where table_schema=@TableSchema";
         public override List<Column> GetColumns(Table table)
         {
             const string sql = @"
-select ordinal_position as Id, column_name as Name, data_type as RawType, column_comment as Comment
-from information_schema.columns where table_schema=@TableSchema
-and table_name=@TableName";
+select col.ordinal_position as Id, col.column_name as Name, data_type as RawType, column_comment as Comment
+, case when cons.constraint_type = 'PRIMARY KEY' then 1 else 0 end as IsPrimaryKey
+, case when cons.constraint_type = 'FOREIGN KEY' then 1 else 0 end as IsForeignKey
+, case when cons.constraint_type = 'UNIQUE' then 1 else 0 end as IsUnique
+, case is_nullable when 'YES' then 1 else 0 end IsNullable
+, character_maximum_length as Length, numeric_precision as ""Precision"", numeric_scale as Scale
+from information_schema.columns col
+left join information_schema.key_column_usage usa on col.table_schema = usa.constraint_schema and col.table_name = usa.table_name and col.column_name = usa.column_name
+left join information_schema.table_constraints cons on usa.table_schema = usa.constraint_schema and usa.table_name = cons.table_name and usa.constraint_name = cons.constraint_name 
+where col.table_schema=@TableSchema
+and col.table_name=@TableName";
             
-            var result = Context.Query<Column>(sql, new { TableSchema = table.Owner, TableName = table.Name });
+            var result = new List<Column>();
+            var cmd = Context.DatabaseObject.GetSqlStringCommand(sql);
+            Context.DatabaseObject.AddInParameter(cmd, "TableSchema", DbType.String, table.Owner);
+            Context.DatabaseObject.AddInParameter(cmd, "TableName", DbType.String, table.Name);
+
+            using (var reader = Context.ExecuteReader(cmd))
+            {
+                while (reader.Read())
+                {
+                    var column = Fill(reader);
+
+                    result.Add(column);
+                }
+            }
 
             return result;
         }
 
         public override Type GetType(string rawType, short precision, short scale, bool isNullable)
         {
-            return typeof(Object);
+            if (String.IsNullOrEmpty(rawType))
+            {
+                throw new ArgumentException("The rawType is null or empty.", "rawType");
+            }
+            
+            switch (rawType.ToLower())
+            {
+                case "bit":
+                case "boolean":
+                    return GetTypeOf<Boolean>(isNullable);
+                case "tinyint":
+                    return GetTypeOf<Byte>(isNullable);
+                case "smallint":
+                case "year":
+                    return GetTypeOf<Int16>(isNullable);
+                case "int":
+                case "integer":
+                case "mediumint":
+                    return GetTypeOf<Int32>(isNullable);
+                case "bigint":
+                    return GetTypeOf<Int64>(isNullable);
+                case "float":
+                    return GetTypeOf<Single>(isNullable);
+                case "double":
+                    return GetTypeOf<Double>(isNullable);
+                case "decimal":
+                    return GetTypeOf<Decimal>(isNullable);
+                case "date":
+                case "time":
+                case "timestamp":
+                case "datetime":
+                    return GetTypeOf<DateTime>(isNullable);
+                case "char":
+                case "varchar":
+                case "longtext":
+                case "text":
+                    return typeof(String);
+                case "binary":
+                case "varbinary":
+                case "mediumblob":
+                case "blob":
+                case "longblob":
+                    return typeof(Byte[]);
+                default:
+                    return typeof(Object);
+            }
         }
 
         public override DbType GetDbType(string rawType, short precision, short scale)
         {
-            return DbType.Object;
+            switch (rawType.ToLower())
+            {
+                case "bit":
+                case "boolean":
+                    return DbType.Boolean;
+                case "tinyint":
+                    return DbType.Byte;
+                case "smallint":                
+                    return DbType.Int16;
+                case "int":
+                case "integer":
+                case "mediumint":
+                    return DbType.Int32;
+                case "bigint":
+                    return DbType.Int64;
+                case "float":
+                    return DbType.Single;
+                case "double":
+                    return DbType.Double;
+                case "decimal":
+                    return DbType.Decimal;
+                case "year":
+                case "date":
+                case "time":
+                case "timestamp":
+                case "datetime":
+                    return DbType.DateTime;
+                case "char":
+                case "varchar":
+                case "longtext":
+                case "text":
+                    return DbType.String;
+                case "binary":
+                case "varbinary":
+                case "blob":
+                case "longblob":
+                    return DbType.Binary;
+                default:
+                    return DbType.Object;
+            }
         }
     }
 }
