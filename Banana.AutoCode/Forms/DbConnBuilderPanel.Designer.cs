8d26537406ee047e4d0b498a35b4b285bdf5fd3b
YuanRui <yuanrui@live.cn>
2020-04-23 17:33:22 +0800

    Add DbConnBuilderPanel

---
+++ Banana.AutoCode/Forms/DbConnBuilderPanel.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Banana.AutoCode.Forms
+{
+    partial class DbConnBuilderPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboDataProvider = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtInstance = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Provider:";
+            // 
+            // cboDataProvider
+            // 
+            this.cboDataProvider.FormattingEnabled = true;
+            this.cboDataProvider.Items.AddRange(new object[] {
+            "Sql Server",
+            "MySql",
+            "Oracle",
+            "SQLite"});
+            this.cboDataProvider.Location = new System.Drawing.Point(91, 29);
+            this.cboDataProvider.Name = "cboDataProvider";
+            this.cboDataProvider.Size = new System.Drawing.Size(100, 20);
+            this.cboDataProvider.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Server:";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(91, 66);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(157, 21);
+            this.txtServer.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "User:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(91, 176);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(157, 21);
+            this.txtUser.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(91, 209);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(157, 21);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(91, 102);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(50, 21);
+            this.txtPort.TabIndex = 9;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(40, 287);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 10;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(121, 287);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(202, 287);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(91, 245);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(157, 21);
+            this.txtName.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Instance:";
+            // 
+            // txtInstance
+            // 
+            this.txtInstance.Location = new System.Drawing.Point(91, 140);
+            this.txtInstance.Name = "txtInstance";
+            this.txtInstance.Size = new System.Drawing.Size(157, 21);
+            this.txtInstance.TabIndex = 16;
+            // 
+            // DbConnBuilderPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 350);
+            this.Controls.Add(this.txtInstance);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboDataProvider);
+            this.Controls.Add(this.label1);
+            this.Name = "DbConnBuilderPanel";
+            this.Text = "Create Connection String";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboDataProvider;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtInstance;
+    }
+}
---
+++ Banana.AutoCode/Forms/DbConnBuilderPanel.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.SQLite;
+using System.Diagnostics;
+
+namespace Banana.AutoCode.Forms
+{
+    public partial class DbConnBuilderPanel : Form
+    {
+        const string SqlServer = "Sql Server";
+        const string MySql = "MySql";
+        const string Oracle = "Oracle";
+        const string SQLite = "SQLite";
+
+        protected class ViewModel
+        {
+            public string Provider { get; set; }
+
+            public string Server { get; set; }
+
+            public int Port { get; set; }
+
+            public string User { get; set; }
+
+            public string Password { get; set; }
+
+            public string Name { get; set; }
+
+            public string Instance { get; set; }
+        }
+
+        public DbConnBuilderPanel()
+        {
+            InitializeComponent();
+        }
+
+        private DbConnectionStringBuilder GetDbConnectionStringBuilder(string provider)
+        {
+            switch (provider)
+            {
+                case "Sql Server":
+                    return new System.Data.SqlClient.SqlConnectionStringBuilder();
+                case "MySql":
+                    return new MySql.Data.MySqlClient.MySqlConnectionStringBuilder();
+                case "Oracle":
+                    return new Oracle.ManagedDataAccess.Client.OracleConnectionStringBuilder();
+                case "SQLite":
+                    return new System.Data.SQLite.SQLiteConnectionStringBuilder();
+
+                default:
+                    break;
+            }
+            return null;
+        }
+
+        private string GetSqlServerConnectionString(ViewModel model)
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder.InitialCatalog = model.Instance;
+            builder.DataSource = model.Server;
+            builder.UserID = model.User;
+            builder.Password = model.Password;
+            
+            return builder.ToString();
+        }
+
+        private string GetMySqlConnectionString(ViewModel model)
+        {
+            var builder = new MySqlConnectionStringBuilder();
+            builder.Server = model.Server;
+            builder.Port = (uint)model.Port;
+            builder.Database = model.Instance;
+            builder.UserID = model.User;
+            builder.Password = model.Password;
+
+            return builder.ToString();
+        }
+
+        private string GetOracleConnectionString(ViewModel model)
+        {
+            var builder = new OracleConnectionStringBuilder();
+            builder.DataSource = $"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={model.Server})(PORT={model.Port}))(CONNECT_DATA=(SERVICE_NAME={model.Instance})))";
+            builder.UserID = model.User;
+            builder.Password = model.Password;
+
+            return builder.ToString();
+        }
+
+        private string GetSQLiteConnectionString(ViewModel model)
+        {
+            var builder = new SQLiteConnectionStringBuilder();
+            builder.DataSource = model.Instance;
+            if (! string.IsNullOrWhiteSpace(model.Password))
+            {
+                builder.Password = model.Password;
+            }
+
+            return builder.ToString();
+        }
+
+        private ConnectionStringSettings BuildSettings()
+        {
+            ConnectionStringSettings settings = new ConnectionStringSettings();
+            var model = GetModel();
+            settings.Name = model.Name;
+
+            switch (model.Provider)
+            {
+                case SqlServer:
+                    settings.ConnectionString = GetSqlServerConnectionString(model);
+                    settings.ProviderName = "System.Data.SqlClient";
+                    break;
+                case MySql:
+                    settings.ConnectionString = GetMySqlConnectionString(model);
+                    settings.ProviderName = "MySql.Data.MySqlClient";
+                    break;
+                case Oracle:
+                    settings.ConnectionString = GetOracleConnectionString(model);
+                    settings.ProviderName = "Oracle.ManagedDataAccess.Client";
+                    break;
+                case SQLite:
+                    settings.ConnectionString = GetSQLiteConnectionString(model);
+                    settings.ProviderName = "System.Data.SQLite";
+                    break;
+                default:
+                    break;
+            }
+            return settings;
+        }
+
+        private ViewModel GetModel()
+        {
+            var model = new ViewModel();
+            model.Provider = cboDataProvider.SelectedText;
+            model.Server = txtServer.Text;
+            model.User = txtUser.Text;
+            model.Password = txtPassword.Text;
+            var port = 0;
+            int.TryParse(txtPort.Text, out port);
+            model.Port = port;
+            model.Name = txtName.Text;
+
+            return model;
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            var settings = BuildSettings();
+            if (settings == null)
+            {
+                return;
+            }
+            var success = false;
+            try
+            {
+                var factory = DbProviderFactories.GetFactory(settings.ProviderName);
+                using (var conn = factory.CreateConnection())
+                {
+                    conn.ConnectionString = settings.ConnectionString;
+                    conn.Open();
+                    success = conn.State == ConnectionState.Open;
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+
+            MessageBox.Show(success ? "Test Success" : "Test Fail");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
